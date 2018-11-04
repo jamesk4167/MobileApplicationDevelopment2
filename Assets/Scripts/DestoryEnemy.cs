f9e4446be5624caa74dbda4bf9a0d7e232f99b33
G00341457 <G00341457@gmit.ie>
2018-11-04 18:59:24 +0000

    Added Destroy Enemy
    Had to change the col.gameObject.name to
    col.gameObject.tag to get the destroy working for clones. also had to add line to detroy Bullet when hits clones

--- Assets/Scripts/DestoryEnemy.cs
+++ Assets/Scripts/DestoryEnemy.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class DestoryEnemy : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col){
-		if(col.gameObject.name == "Bullet"){
+		if(col.gameObject.tag == "Bullet"){
 			
 			Object.Destroy(this.gameObject);
+			Object.Destroy(col.gameObject);
             col.gameObject.SetActive(false);
 	}
 }
